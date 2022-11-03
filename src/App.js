2f82565488e85d943bd0f0cd3d016e06db11db1c
marcel-codett <uwakwemgodspower99@gmail.comgit config --global user.name>
2022-11-03 05:50:53 -0700

    refactor: created pages to suit routing features.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import "./App.css";
-
-import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+import "./../node_modules/@syncfusion/ej2-base/styles/material.css";
 import "../node_modules/@syncfusion/ej2-react-popups/styles/material.css";
-import Header from "./components/Header";
-import { items } from "../src/assets/links";
-import slack from "../src/assets/slack.png";
-import github from "../src/assets/github.png";
-// import { FaSlack, FaGithub } from "react-icons/fa";
-import LinkTree from "./components/LinkTree";
-import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Contact from "./components/Contact";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="">
       <BrowserRouter>
-        <div
-          className="dark:bg-main-dark-bg 
-      overflow-x-hidden py-20 grid place-items-center h-max"
-        >
-          <Header />
-          <LinkTree cardItems={items} />
-          <div className="flex items-center text-[#667085] space-x-10 my-20">
-            <a href="https://hng.slack.com/archives/D048FS4CCKC">
-              <img
-                className="w-7 h-7 
-              dark:text-white
-             object-cover"
-                src={slack}
-                alt="slack"
-              />
-            </a>
-            <a href="https://github.com/marcel-codett">
-              <img
-                className="
-            dark:text-white w-7 h-7 object-cover"
-                src={github}
-                alt="github"
-              />
-            </a>
-
-            <Routes>
-              <Route path="contact" element={<Contact />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "../components/Header";
+import { items } from "../assets/links";
+import slack from "../assets/slack.png";
+import github from "../assets/github.png";
+// import { FaSlack, FaGithub } from "react-icons/fa";
+import LinkTree from "../components/LinkTree";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="">
+        <div
+          className="dark:bg-main-dark-bg 
+      overflow-x-hidden py-20 grid place-items-center h-max"
+        >
+          <Header />
+          <LinkTree cardItems={items} />
+          <div className="flex items-center text-[#667085] space-x-10 my-20">
+            <a href="https://hng.slack.com/archives/D048FS4CCKC">
+              <img
+                className="w-7 h-7 
+              dark:text-white
+             object-cover"
+                src={slack}
+                alt="slack"
+              />
+            </a>
+            <a href="https://github.com/marcel-codett">
+              <img
+                className="
+            dark:text-white w-7 h-7 object-cover"
+                src={github}
+                alt="github"
+              />
+            </a>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
